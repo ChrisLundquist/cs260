a9b8e23ddfe12697307049ba538719f36b09c136
U-Simulacrum\Durandal <Durandal@Simulacrum.(none)>
2012-09-16 01:24:31 -0700

    Works on windows

--- assignment_1/client.cpp
+++ assignment_1/client.cpp
@@ -1,81 +1,98 @@
 #include "network.h"
 
 int main(int argc, char *argv[]) {
-    char done = 0;
-    int sockfd, sent_size, recv_size;
-    char buffer[BUFFER_SIZE];
-    struct addrinfo hints, *servinfo, *p;
-    int rv;
-    char port[PORT_LENGTH];
-    char remote[INET6_ADDRSTRLEN];
+  char done = 0;
+  int sockfd, sent_size, recv_size;
+  char buffer[BUFFER_SIZE];
+  struct addrinfo hints, *servinfo, *p;
+  int rv;
+  char port[PORT_LENGTH];
+  char remote[INET6_ADDRSTRLEN];
 
-    if(argc == 1) {
-        read_config(remote, port);
-        printf("Configured for Remote: '%s'\n", remote);
-        printf("Configured for Port: '%s'\n", port);
-    } else if(argc == 3) {
-        //remote = argv[1];
-        //port = argv[2];
-    } else {
-        printf("Provide no arguments to read config.txt or provide a hostname and port\n");
-        printf("%s host port", argv[0]);
+#ifdef _WIN32
+  WSADATA wsa_data;
+  SecureZeroMemory(&wsa_data, sizeof(wsa_data));
+  rv = WSAStartup(MAKEWORD(2,2), &wsa_data);
+  if(rv != 0) {
+    fprintf(stderr, "WSAStartup failed\n");
+    exit(-666);
+  }
+#else
+  int yes = 1;
+#endif
+
+  if(argc == 1) {
+    read_config(remote, port);
+    printf("Configured for Remote: '%s'\n", remote);
+    printf("Configured for Port: '%s'\n", port);
+  } else if(argc == 3) {
+    //remote = argv[1];
+    //port = argv[2];
+  } else {
+    printf("Provide no arguments to read config.txt or provide a hostname and port\n");
+    printf("%s host port", argv[0]);
+  }
+
+  memset(&hints, 0, sizeof hints);
+  hints.ai_family = AF_UNSPEC;
+  hints.ai_socktype = SOCK_STREAM;
+
+  if((rv = getaddrinfo(remote, port, &hints, &servinfo)) != 0) {
+    fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+    return 1;
+  }
+
+  // loop through all the results and connect to the first we can
+  for(p = servinfo; p != NULL; p = p->ai_next) {
+    if((sockfd = socket(p->ai_family, p->ai_socktype,
+            p->ai_protocol)) == -1) {
+      perror("client: socket");
+      continue;
+    }
+    if(connect(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+      close(sockfd);
+      perror("client: connect");
+      continue;
+    }
+    break;
+  }
+
+  freeaddrinfo(servinfo); // all done with this structure
+
+  if(p == NULL) {
+    fprintf(stderr, "client: failed to connect\n");
+    return 2;
+  }
+
+
+#ifdef _WIN32
+  // Windows doesn't set this since it isn't explicitly asked for
+#else
+  inet_ntop(p->ai_family, get_in_addr((struct sockaddr *)p->ai_addr),
+      remote, sizeof remote);
+  printf("client: connecting to %s\n", remote);
+#endif
+
+  while(!done) {
+    fgets(buffer, BUFFER_SIZE, stdin);
+
+    sent_size = strlen(buffer);
+
+    sent_size = send(sockfd, buffer, sent_size, 0);
+    if( sent_size == -1) {
+      perror("send");
+      break;
     }
 
-    memset(&hints, 0, sizeof hints);
-    hints.ai_family = AF_UNSPEC;
-    hints.ai_socktype = SOCK_STREAM;
-
-    if((rv = getaddrinfo(remote, port, &hints, &servinfo)) != 0) {
-        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
-        return 1;
+    recv_size = recv(sockfd, buffer, BUFFER_SIZE-1, 0);
+    if( recv_size == -1) {
+      perror("recv");
+      break;
     }
 
-    // loop through all the results and connect to the first we can
-    for(p = servinfo; p != NULL; p = p->ai_next) {
-        if((sockfd = socket(p->ai_family, p->ai_socktype,
-                        p->ai_protocol)) == -1) {
-            perror("client: socket");
-            continue;
-        }
-        if(connect(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
-            close(sockfd);
-            perror("client: connect");
-            continue;
-        }
-        break;
-    }
-
-    freeaddrinfo(servinfo); // all done with this structure
-
-    if(p == NULL) {
-        fprintf(stderr, "client: failed to connect\n");
-        return 2;
-    }
-
-    inet_ntop(p->ai_family, get_in_addr((struct sockaddr *)p->ai_addr),
-            remote, sizeof remote);
-    printf("client: connecting to %s\n", remote);
-
-    while(!done) {
-        fgets(buffer, BUFFER_SIZE, stdin);
-
-        sent_size = strlen(buffer);
-
-        sent_size = send(sockfd, buffer, sent_size, 0);
-        if( sent_size == -1) {
-            perror("send");
-            break;
-        }
-
-        recv_size = recv(sockfd, buffer, BUFFER_SIZE-1, 0);
-        if( recv_size == -1) {
-            perror("recv");
-            break;
-        }
-
-        buffer[recv_size] = '\0';
-        printf("client received: %s", buffer);
-    }
-    close(sockfd);
-    return 0;
+    buffer[recv_size] = '\0';
+    printf("client received: %s", buffer);
+  }
+  close(sockfd);
+  return 0;
 }
--- assignment_1/server.cpp
+++ assignment_1/server.cpp
@@ -2,100 +2,108 @@
 #include "network.h"
 
 int main(void) {
-    char listening_port[PORT_LENGTH];
-    char buffer[BUFFER_SIZE];
-    int recv_size, send_size;
-    int sockfd, new_fd;  // listen on sock_fd, new connection on new_fd
-    struct addrinfo hints, *servinfo, *p;
-    struct sockaddr_storage their_addr; // connector's address information
-    socklen_t sin_size;
+  char listening_port[PORT_LENGTH];
+  char buffer[BUFFER_SIZE];
+  int recv_size, send_size;
+  int sockfd, new_fd;  // listen on sock_fd, new connection on new_fd
+  struct addrinfo hints, *servinfo, *p;
+  struct sockaddr_storage their_addr; // connector's address information
+  socklen_t sin_size;
+  char address[INET6_ADDRSTRLEN];
+  int rv;
 
 #ifdef _WIN32
-	char yes = '1';
+  char yes = '1';
+  WSADATA wsa_data;
+  SecureZeroMemory(&wsa_data, sizeof(wsa_data));
+  rv = WSAStartup(MAKEWORD(2,2), &wsa_data);
+  if(rv != 0) {
+    fprintf(stderr, "WSAStartup failed\n");
+    exit(-666);
+  }
 #else
-    int yes = 1;
+  int yes = 1;
 #endif
-    char address[INET6_ADDRSTRLEN];
-    int rv;
 
-    read_config(address, listening_port);
-    printf("Configured for Port: '%s'\n", listening_port);
 
-    memset(&hints, 0, sizeof hints);
-    hints.ai_family = AF_UNSPEC;
-    hints.ai_socktype = SOCK_STREAM;
-    hints.ai_flags = AI_PASSIVE; // use my IP
+  read_config(address, listening_port);
+  printf("Configured for Port: '%s'\n", listening_port);
 
-    if((rv = getaddrinfo(NULL, listening_port, &hints, &servinfo)) != 0) {
-        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
-        return 1;
+  memset(&hints, 0, sizeof hints);
+  hints.ai_family = AF_UNSPEC;
+  hints.ai_socktype = SOCK_STREAM;
+  hints.ai_flags = AI_PASSIVE; // use my IP
+
+  if((rv = getaddrinfo(NULL, listening_port, &hints, &servinfo)) != 0) {
+    fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+    return 1;
+  }
+
+  // loop through all the results and bind to the first we can
+  for(p = servinfo; p != NULL; p = p->ai_next) {
+    if((sockfd = socket(p->ai_family, p->ai_socktype,
+            p->ai_protocol)) == -1) {
+      perror("server: socket");
+      continue;
+    }
+    if(setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes,
+          sizeof(yes)) == -1) {
+      perror("setsockopt");
+      exit(1);
+    }
+    if(bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+      close(sockfd);
+      perror("server: bind");
+      continue;
+    }
+    break;
+  }
+
+  if(p == NULL)  {
+    fprintf(stderr, "server: failed to bind\n");
+    return 2;
+  }
+
+  freeaddrinfo(servinfo); // all done with this structure
+  if(listen(sockfd, BACKLOG) == -1) {
+    perror("listen");
+    exit(1);
+  }
+
+  printf("server: waiting for connections...\n");
+  while(1) {  // main accept() loop
+
+    sin_size = sizeof their_addr;
+    new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
+
+    if(new_fd == -1) {
+      perror("accept");
+      continue;
     }
 
-    // loop through all the results and bind to the first we can
-    for(p = servinfo; p != NULL; p = p->ai_next) {
-        if((sockfd = socket(p->ai_family, p->ai_socktype,
-                        p->ai_protocol)) == -1) {
-            perror("server: socket");
-            continue;
-        }
-        if(setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes,
-                    sizeof(yes)) == -1) {
-            perror("setsockopt");
-            exit(1);
-        }
-        if(bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
-            close(sockfd);
-            perror("server: bind");
-            continue;
-        }
+    inet_ntop(their_addr.ss_family,
+        get_in_addr((struct sockaddr *)&their_addr),
+        address, sizeof address);
+
+    printf("server: got connection from %s\n", address);
+
+    do {
+      recv_size = recv(new_fd, buffer, BUFFER_SIZE - 1, 0);
+
+      if(recv_size == -1) {
+        perror("recv");
         break;
-    }
+      }
 
-    if(p == NULL)  {
-        fprintf(stderr, "server: failed to bind\n");
-        return 2;
-    }
+      buffer[recv_size] = NULL;
+      printf("server: got message from %s: %s\n", address, buffer);
 
-    freeaddrinfo(servinfo); // all done with this structure
-    if(listen(sockfd, BACKLOG) == -1) {
-        perror("listen");
-        exit(1);
-    }
+      send_size = send(new_fd, buffer, recv_size, 0);
+      if( send_size == -1 || send_size != recv_size)
+        perror("send");
+    } while(recv_size != 0);
 
-    printf("server: waiting for connections...\n");
-    while(1) {  // main accept() loop
-
-        sin_size = sizeof their_addr;
-        new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
-
-        if(new_fd == -1) {
-            perror("accept");
-            continue;
-        }
-
-        inet_ntop(their_addr.ss_family,
-                get_in_addr((struct sockaddr *)&their_addr),
-                address, sizeof address);
-
-        printf("server: got connection from %s\n", address);
-
-        do {
-            recv_size = recv(new_fd, buffer, BUFFER_SIZE - 1, 0);
-
-            if(recv_size == -1) {
-                perror("recv");
-                break;
-            }
-
-            buffer[recv_size] = NULL;
-            printf("server: got message from %s: %s\n", address, buffer);
-
-            send_size = send(new_fd, buffer, recv_size, 0);
-            if( send_size == -1 || send_size != recv_size)
-                perror("send");
-        } while(recv_size != 0);
-
-        close(new_fd);  // parent doesn't need this
-    }
-    return 0;
+    close(new_fd);  // parent doesn't need this
+  }
+  return 0;
 }
